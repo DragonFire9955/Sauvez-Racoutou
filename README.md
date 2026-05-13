c386cf77dd721e4479644a2736a8015922d11d19
Achille Valentin-Lescanne <achille.valesc@gmail.com>
2026-05-13 14:52:19 +0200

    ajout test map

--- README.md
+++
@@ -1,1 +0,0 @@
-# Sauvez-Poutou
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.sae_dev {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.sae_dev to javafx.fxml;
+    exports com.example.sae_dev;
+}
