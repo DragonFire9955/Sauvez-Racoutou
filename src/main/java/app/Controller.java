a25418e2abe47249fefcf6e9ff96e49d3aed1b52
DragonFire9955 <pinconthomas16@gmail.com>
2026-05-14 14:08:38 +0200

    Correction des fps

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -56,7 +56,7 @@ public class Controller implements Initializable {
 
         KeyFrame kf = new KeyFrame(
                 // on définit le FPS (nbre de frame par seconde)
-                Duration.seconds(0.0017),
+                Duration.seconds(0.017),
                 // on définit ce qui se passe à chaque frame
                 // c'est un eventHandler d'ou le lambda
                 (ev ->{
